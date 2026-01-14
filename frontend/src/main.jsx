4dc9d21a0edbfdf615ebcb577cb6997efd64c918
kaif304 <152968251+kaif304@users.noreply.github.com>
2026-01-15 00:16:04 +0530

    tailwind setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
+import './style.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div>
-      <h1>Hello!</h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   </StrictMode>,
 )
