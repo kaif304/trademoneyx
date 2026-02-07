a76c9c0b83725f236b57dc34d57b8fdad7b2cc4c
kaif304 <152968251+kaif304@users.noreply.github.com>
2026-02-08 00:07:11 +0530

    fix: Add responsiveness to About page

--- frontend/src/landing_page/about/Hero.jsx
+++ frontend/src/landing_page/about/Hero.jsx
@@ -3,19 +3,16 @@ import React from 'react'
 function Hero( ) {
     return(
         <section className='hero-section'>
-            <div className='grid grid-rows-[auto_1fr] gap-2 px-32'>
-
-                <div className='grid grid-cols-12'>
-                    <div className='col-span-12 px-4 py-20'>
-                        <div className='text-center text-3xl text-gray-800 font-semibold'>
-                            <h1>We pioneered the discount broking model in India.</h1>
-                            <h1>Now, we are breaking ground with our technology.</h1>
-                        </div>
+            <div className='px-2 sm:px-12 md:px-18 lg:px-32'>
+                <div className='px-4 py-4 sm:py-8 md:py-18'>
+                    <div className='text-center sm:text-2xl md:text-3xl text-gray-800 font-semibold'>
+                        <h1>We pioneered the discount broking model in India.</h1>
+                        <h1>Now, we are breaking ground with our technology.</h1>
                     </div>
                 </div>
 
-                <div className='grid grid-cols-12 gap-4 pt-20 px-24 mb-8 border-t border-gray-300 text-gray-700 text-lg/relaxed'>
-                    <div className='col-span-6 grid gap-4 px-8'>
+                <div className='grid grid-cols-12 md:gap-8 lg:gap-16 pt-6 sm:pt-10 md:pt-18 px-1 sm:px-2 md:px-4 lg:px-8 md:mb-8 border-t border-gray-300 text-gray-700 text-sm sm:text-md md:text-lg'>
+                    <div className='col-span-12 md:col-span-6 grid gap-4'>
                         <p>
                             We kick-started operations on the 15th of August, 2010 
                             with the goal of breaking all barriers that traders and 
@@ -32,7 +29,7 @@ function Hero( ) {
                             of all Indian retail trading volumes.
                         </p>
                     </div>
-                    <div className='col-span-6 grid gap-4 px-8'>
+                    <div className='col-span-12 md:col-span-6 grid gap-4 hidden md:block'>
                         <p>
                             In addition, we run a number of popular open online educational and 
                             community initiatives to empower traders and investors.
--- frontend/src/landing_page/about/Team.jsx
+++ frontend/src/landing_page/about/Team.jsx
@@ -3,27 +3,24 @@ import React from 'react'
 function Team( ) {
     return(
         <section className='team-section'>
-            <div className='grid grid-rows-[auto_1fr] gap-2 px-32 mt-16'>
-
-                <div className='grid grid-cols-12 border-t border-gray-300'>
-                    <div className='col-span-12 px-4 py-16'>
-                        <div className='text-center text-3xl text-gray-800 font-semibold'>
-                            <h1>People</h1>
-                        </div>
+            <div className='px-2 sm:px-12 md:px-18 lg:px-32 mt-16'>
+                <div className='border-t border-gray-300'>
+                    <div className='text-center text-2xl md:text-3xl py-8 sm:py-14 text-gray-800 font-semibold'>
+                        <h1>People</h1>
                     </div>
                 </div>
 
-                <div className='grid grid-cols-12 gap-8 px-24 mb-8 text-gray-700 text-lg/relaxed'>
-                    <div className='col-span-6 grid place-content-center gap-6'>
+                <div className='grid grid-cols-12 gap-8 mb-8 text-gray-700'>
+                    <div className='col-span-12 lg:col-span-6 grid place-content-center gap-6'>
 
-                        <img className='rounded-full h-80' src="media/images/kaif.png" alt="Team Image" />
+                        <img className='rounded-full h-50 sm:h-80' src="media/images/kaif.png" alt="Team Image" />
 
                         <div className='text-xl text-center font-semibold text-gray-600'>
                             <h4>Mohd Kaif</h4>
                             <h4 className='text-sm mt-1'>Founder, CEO</h4>
                         </div>
                     </div>
-                    <div className='col-span-6 grid gap-6 place-content-start px-8'>
+                    <div className='col-span-12 lg:col-span-6 grid gap-4 sm:gap-6 place-content-start text-center lg:text-left text-sm md:text-md lg:text-lg leading-relaxed'>
                         <p>
                             Mohd Kaif bootstrapped and founded TradeMoneyX in 2025 to overcome 
                             the hurdles he faced during his decade long stint as a trader.
